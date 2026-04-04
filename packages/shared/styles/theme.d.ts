b10e0555b94e28a4a1df6045e9b2eb0651d72ae7
Kenneth Yoo <kenny@Kennethui-Macmini.local>
2026-04-05 07:46:09 +0900

    fix(shared): add @types/node and theme.d.ts to resolve tsc errors

---
+++ packages/shared/styles/theme.d.ts
@@ -0,0 +1,14 @@
+export declare const sharedTheme: {
+  colors: {
+    brand: {
+      50: string;
+      100: string;
+      500: string;
+      600: string;
+      700: string;
+    };
+  };
+  fontFamily: {
+    sans: string[];
+  };
+};
